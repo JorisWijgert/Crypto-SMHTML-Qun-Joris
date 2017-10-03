8d6fc6d3bd4ced5a2c68e968a2275160af037474
Joris van de Wijgert <jvdwijgert@hotmail.com>
2017-10-03 16:35:24 +0200

    trials in visual studio

---
+++ Trial/webapitrial/webapitrial/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapitrial.Controllers
+{
+    public class FirstController : ApiController
+    {
+        [HttpGet]
+        public string returnThings()
+        {
+            return "Hallo Qun";
+        }
+
+        [HttpPost]
+        public string postAndReturn()
+        {
+            return "Hey hey";
+        }
+    }
+}
