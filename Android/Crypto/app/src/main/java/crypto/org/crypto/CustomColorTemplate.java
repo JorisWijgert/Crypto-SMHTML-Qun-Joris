1f8052ce211971339da592bbf2e58c8a3fceda37
Qunfong <qunfong@live.nl>
2017-11-22 13:29:29 +0100

    Legend in piechart finally works. Uses amounts as legend

---
+++ Android/Crypto/app/src/main/java/crypto/org/crypto/CustomColorTemplate.java
@@ -0,0 +1,23 @@
+package crypto.org.crypto;
+
+import android.graphics.Color;
+
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+/**
+ * Created by Qunfo on 22-Nov-17.
+ */
+
+public class CustomColorTemplate {
+
+    public static final int[] Colors = {
+            Color.rgb(255, 179, 0), Color.rgb(128, 62, 117), Color.rgb(255, 104, 0),
+            Color.rgb(166, 189, 215), Color.rgb(193, 0, 32), Color.rgb(206, 162, 98),
+            Color.rgb(129, 112, 102), Color.rgb(0, 125, 52), Color.rgb(246, 118, 142),
+            Color.rgb(0, 83, 138), Color.rgb(255, 122, 92), Color.rgb(83, 55, 122),
+            Color.rgb(255, 142, 0), Color.rgb(179, 40, 81), Color.rgb(244, 200, 0),
+            Color.rgb(127, 24, 13), Color.rgb(147, 170, 0), Color.rgb(89, 51, 21),
+            Color.rgb(241, 58, 19), Color.rgb(35, 44, 22)
+    };
+
+}
