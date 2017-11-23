09adb549d5a475a407b7bcb67a3b8175555cfc6e
Qunfong <qunfong@live.nl>
2017-11-23 12:32:56 +0100

    Created GraphFragment which will show all charts. Candlegraph cardview opens a graphfragment. In GraphCardview fragment it shows 5 cardviews. Last 2 cardviews are TBD.

---
+++ Android/Crypto/app/src/main/java/crypto/org/crypto/GraphFragment.java
@@ -0,0 +1,26 @@
+package crypto.org.crypto;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class GraphFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_graph, container, false);
+
+        return view;
+    }
+}
