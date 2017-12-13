02ea811f2c6df4f8f9a859fe1be6bbbd612685a3
Qunfong <Qunfong@live.nl>
2017-12-13 04:33:32 -0800

    Initial Commit

---
+++ iOS/SimpleMeal/SimpleMeal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleMeal
+//
+//  Created by Qunfong on 13/12/2017.
+//  Copyright © 2017 Qunfong. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
