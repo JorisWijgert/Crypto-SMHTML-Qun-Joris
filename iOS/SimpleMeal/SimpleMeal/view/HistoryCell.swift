832686be87c6044f2d28b4c9783d1bd0830cbf48
Qunfong <Qunfong@live.nl>
2018-01-09 11:42:33 +0100

    Added image to history array. When clicking on the cell it will go to the detailspage

--- iOS/SimpleMeal/SimpleMeal/view/HistoryCell.swift
+++ iOS/SimpleMeal/SimpleMeal/view/HistoryCell.swift
@@ -11,8 +11,31 @@ import UIKit
 class HistoryCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var historyImageView: UIImageView!
     
     func updateUI(recipe: Recipe) {
         titleLabel.text = recipe.Name
+        if let url = URL(string: recipe.ImageUrl){
+            historyImageView.contentMode = .scaleAspectFit
+            downloadImage(url: url)
+        }
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() {
+                self.historyImageView.image = UIImage(data: data)
+            }
+        }
     }
 }
