dad31d6719e8fe1f6ec1b3b24e6fcca158a7b1f5
Joris van de Wijgert <jvdwijgert@hotmail.com>
2018-01-09 10:33:26 +0100

    Video works now!

--- iOS/SimpleMeal/SimpleMeal/controller/RecipeDetailController.swift
+++ iOS/SimpleMeal/SimpleMeal/controller/RecipeDetailController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class RecipeDetailController: UIViewController {
 
+
+    @IBOutlet weak var RecipeImage: UIImageView!
+    
+    @objc func tapDetected(){
+        let videoURL = URL(string: "https://flvpd.vtm.be/videocms/vmma/2015/10/15/201510151531051010034065084005056B7305C0000003648B00000D0F064314.mp4")
+        let player = AVPlayer(url: videoURL!)
+        let playerViewController = VideoPlayer()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let singleTap = UITapGestureRecognizer(target: self, action: Selector("tapDetected"))
+        RecipeImage.isUserInteractionEnabled = true
+        RecipeImage.addGestureRecognizer(singleTap)
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +38,7 @@ class RecipeDetailController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
---
+++ iOS/SimpleMeal/SimpleMeal/controller/VideoPlayer.swift
@@ -0,0 +1,15 @@
+//
+//  VideoPlayer.swift
+//  SimpleMeal
+//
+//  Created by Fhict on 09-01-18.
+//  Copyright © 2018 SimpleMeal. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class VideoPlayer: AVPlayerViewController {
+
+}
